4a643a9fc320dabf771947c5340816a0715d95fe
mkk4726 <mkk4726@naver.com>
2023-07-15 20:46:07 +0900

    ~chapter4

--- src/modules/common/functions.py
+++ src/modules/common/functions.py
@@ -19,3 +19,15 @@ def sigmoid(x):
         [x > 0],
         [lambda i: 1 / (1 + np.exp(-i)), lambda i: np.exp(i) / (1 + np.exp(i))],
     )
+    
+def cross_entropy_error(y, t, is_onehot=True):
+  if y.ndim == 1:
+    t = t.reshape(1, t.size)
+    y = y.reshape(1, y.size)
+    
+  batch_size = y.shape[0]
+  if is_onehot:
+    return -np.sum(t * np.log(y + 1e-7)) / batch_size 
+  else:
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
+  
