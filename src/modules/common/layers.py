5ccaa808b16d6261b33b692cd9a206b2fe797842
mkk4726 <mkk4726@naver.com>
2023-07-16 12:00:59 +0900

    ~chapter 5

---
+++ src/modules/common/layers.py
@@ -0,0 +1,128 @@
+import numpy as np
+
+class SoftmaxWithLoss:
+  def __init__(self):
+    self.loss = None
+    self.y = None
+    self.t = None
+  
+  def softmax(self, a):
+    C = np.max(a)
+    exp_a = np.exp(a - C)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
+  
+  def cross_entropy_error(self, y, t, is_onehot=True):
+    if y.ndim == 1:
+      t = t.reshape(1, t.size)
+      y = y.reshape(1, y.size)
+      
+    batch_size = y.shape[0]
+    if is_onehot:
+      return -np.sum(t * np.log(y + 1e-7)) / batch_size 
+    else:
+      return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
+    
+  def forward(self, x, t):
+    self.t = t
+    self.y = self.softmax(x)
+    self.loss = self.cross_entropy_error(self.y, self.t)
+    return self.loss
+  
+  def backward(self, dout=1):
+    batch_size = self.t.shape[0]
+    dx = (self.y - self.t) / batch_size
+    
+    return dx
+  
+class Affine:
+  def __init__(self, W, b):
+    self.W = W
+    self.b = b
+    self.x = None
+    self.dW = None
+    self.db = None
+    
+  def forward(self, x):
+    self.x = x
+    out = np.dot(x, self.W) + self.b
+    
+    return out
+  
+  def backward(self, dout):
+    dx = np.dot(dout, self.W.T)
+    self.dW = np.dot(self.x.T, dout)
+    self.db = np.sum(dout, axis=0)
+    
+    return dx
+  
+class Sigmoid:
+  def __init__(self):
+    self.out = None
+    
+  def forward(self, x):
+    out = np.piecewise(
+        x,
+        [x > 0],
+        [lambda i: 1 / (1 + np.exp(-i)), lambda i: np.exp(i) / (1 + np.exp(i))],
+    )
+    self.out = out
+    
+    return out
+  
+  def backward(self, dout):
+    dx = dout * (1.0 - self.out) * self.out
+    
+    return dx
+  
+  
+class Relu:
+  def __init__(self):
+    self.mask = None
+    
+  def forward(self, x):
+    self.mask = (x <= 0)
+    out = x.copy()
+    out[self.mask] = 0
+    
+    return out
+    
+  def backward(self, dout):
+    dout[self.mask] = 0
+    dx = dout
+    
+    return dx
+  
+class AddLayer:
+  def __init__(self):
+    pass
+  
+  def forward(self, x, y):
+    out = x + y
+    return out
+  
+  def backward(self, dout):
+    dx = dout * 1
+    dy = dout * 1
+    
+    return dx, dy
+  
+  
+class MultiLayer:
+  def __init__(self):
+    self.x = None
+    self.y = None
+    
+  def forward(self, x, y):
+    self.x = x
+    self.y = y
+    out = x * y
+    
+    return out
+  
+  def backward(self, dout):
+    dx = dout * self.y
+    dy = dout * self.x
+    
+    return dx, dy
